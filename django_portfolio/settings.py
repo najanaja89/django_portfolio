d1d73aa54566ff38299bd32c6e0cc164ecba42ad
Руслан Тё <R.Tyo@astanaballet.local>
2020-06-01 15:22:45 +0600

    added white noise

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -122,8 +122,14 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+
